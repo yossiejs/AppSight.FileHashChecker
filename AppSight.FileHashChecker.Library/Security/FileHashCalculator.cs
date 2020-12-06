c2651bf15b0759e35db4ec475f2607930e232a3e
yossiejs <yossiejs@gmail.com>
2020-12-06 18:25:21 +0900

    fix misc

--- AppSight.FileHashChecker.Library/Security/FileHashCalculator.cs
+++ AppSight.FileHashChecker.Library/Security/FileHashCalculator.cs
@@ -48,7 +48,7 @@ namespace AppSight.FileHashChecker.Library.Security
                         }
 
                     default:
-                        throw new InvalidOperationException($"Specified unknown hash type. hashType={hashType}");
+                        throw new ArgumentOutOfRangeException();
                 }
             }
         }
