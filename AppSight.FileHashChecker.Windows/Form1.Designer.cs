ae6c1eeb4f7920362d4d446faff70ee93856932b
yossiejs <yossiejs@gmail.com>
2020-12-30 16:53:50 +0900

    enable localization

--- AppSight.FileHashChecker.Windows/Form1.Designer.cs
+++ AppSight.FileHashChecker.Windows/Form1.Designer.cs
@@ -29,16 +29,14 @@ namespace AppSight.FileHashChecker.Windows
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.SuspendLayout();
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
